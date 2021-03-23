4698e725d526e477827caa54af3e97daaf56c57d
Anh Nguyen <anguyen@swin.edu.au>
2021-03-23 14:42:13 +1100

    Random Number example with an else if

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace RandomNumberExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+
+            int randomNumber = rand.Next(101);
+
+            if (randomNumber > 50) {
+                System.Console.WriteLine($"{randomNumber} is greater than 50");
+            } else if (randomNumber == 50) {
+                System.Console.WriteLine($"{randomNumber} is equal to 50");
+            } else {
+                System.Console.WriteLine($"{randomNumber} is less than 50");
+            }
+
+        }
+    }
+}
